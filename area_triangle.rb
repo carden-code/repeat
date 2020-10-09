5b56a189b420581c10347fdc427563d2bb6087b8
Carden <carden.ruby@gmail.com>
2020-10-09 20:47:47 +0300

    first commit

---
+++ area_triangle.rb
@@ -0,0 +1,8 @@
+# The program should query the base and
+# height of the triangle and return its area.
+puts 'Please enter the base of the triangle:'
+base = gets.chomp.to_i
+puts 'Please enter the height of the triangle'
+height = gets.chomp.to_i
+area_triangle = 0.5 * base * height
+puts "The area of ​​the triangle is #{area_triangle}"
