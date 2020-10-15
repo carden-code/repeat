c78218d636ddebc0307f5c76ea42793e08be6c96
Carden <carden.ruby@gmail.com>
2020-10-15 20:19:51 +0300

    modification

--- quadratic_equation.rb
+++ quadratic_equation.rb
@@ -6,23 +6,27 @@
 # displays the values of the discriminant and roots on the screen.
 puts 'Solving a quadratic equation.'
 puts "Please enter the coefficients where 'a' is not equal to 0"
-puts 'Please enter the coefficient a:'
-a = gets.chomp.to_i
-puts 'Please enter the coefficient b:'
-b = gets.chomp.to_i
-puts 'Please enter the coefficient c:'
-c = gets.chomp.to_i
+puts 'Please enter the coefficient a = :'
+a = gets.chomp.to_f
+if a.zero?
+  puts 'Not a quadratic equation.'
+  return
+end
+puts 'Please enter the coefficient b = :'
+b = gets.chomp.to_f
+puts 'Please enter the coefficient c = :'
+c = gets.chomp.to_f
 
 discriminant = b**2 - (4 * a * c)
 puts "Discriminant: #{discriminant}"
 if discriminant.positive?
-  first = (-b + Math.sqrt(discriminant)) / (2 * a)
-  second = (-b - Math.sqrt(discriminant)) / (2 * a)
-  puts "First root of the equation: #{first}"
-  puts "Second root of the equation: #{second}"
+  x1 = (-b + Math.sqrt(discriminant)) / (2 * a)
+  x2 = (-b - Math.sqrt(discriminant)) / (2 * a)
+  puts "First root of the equation: #{x1}"
+  puts "Second root of the equation: #{x2}"
 elsif discriminant.zero?
-  root = -b / (2 * a)
-  puts "Root of the equation: #{root}"
+  x = -b / (2 * a)
+  puts "Root of the equation: #{x}"
 else
-  puts 'No roots'
+  puts 'No roots.'
 end
