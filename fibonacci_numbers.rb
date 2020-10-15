7dd2a03e11fefb07788ed2e768a4fa170c7b0d80
Carden <carden.ruby@gmail.com>
2020-10-15 20:38:52 +0300

    fibonacci

---
+++ fibonacci_numbers.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Fill the array with Fibonacci numbers up to 100.
+fibonacci = [0, 1]
+next_element = 1
+while next_element <= 100
+  fibonacci << next_element
+  next_element = fibonacci[-1] + fibonacci[-2]
+end
+puts "Fibonacci numbers up to 100: #{fibonacci}"
